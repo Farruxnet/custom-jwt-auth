237583c42b8a1095112e88844d7736f399d19aa5
fzdglobal <fzdglobalteam@gmail.com>
2022-09-20 13:17:52 +0500

    Initial commit

---
+++ users/admin.py
@@ -0,0 +1,66 @@
+from django.contrib import admin
+from . models import User
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django import forms
+from django.core.exceptions import ValidationError
+
+class UserCreationForm(forms.ModelForm):
+
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'name', 'phone_number')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Parol mos kelmadi")
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(help_text=("<a href=\"../password/\">Foydalanuvchi parolini o'zgartirish!</a>."))
+    class Meta:
+        model = User
+        fields = ('name', 'username', 'phone_number', 'is_active', 'is_admin')
+
+    def clean_password(self):
+
+        return self.initial["password"]
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+
+    list_display = ('username', 'name', 'phone_number', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('name', 'phone_number')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'name', 'phone_number', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('username', 'name', 'phone_number',)
+    ordering = ('id',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
---
+++ users/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.utils import timezone
+from django.conf import settings
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('Users must have an username')
+
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None):
+        user = self.create_user(
+            username,
+            password=password,
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length = 64, null = True, blank = True)
+    username = models.CharField(max_length = 32, unique = True)
+    phone_number = models.CharField(max_length = 12, unique = True, null = True, blank = True)
+    create_at = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    class Meta:
+        verbose_name="User"
+        verbose_name_plural="Users"
+
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    
